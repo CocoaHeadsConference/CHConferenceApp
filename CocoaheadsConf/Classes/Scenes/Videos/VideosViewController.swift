41fb08ced145567dda8f3b2909cd2ff87f1733cb
Guilherme Rambo <insidegui@gmail.com>
2016-11-23 16:51:38 -0200

    Styled videos table, showing talk details when video is selected

--- CocoaheadsConf/Classes/Scenes/Videos/VideosViewController.swift
+++ CocoaheadsConf/Classes/Scenes/Videos/VideosViewController.swift
@@ -8,14 +8,18 @@
 
 import UIKit
 
-class VideosViewController: UITableViewController {
+class VideosViewController: UIViewController {
 
-    private struct Storyboard {
+    fileprivate struct Storyboard {
         static let cell = "videoCell"
     }
     
+    @IBOutlet weak var tableView: UITableView!
+    
     var videos: [VideoModel] = [] {
         didSet {
+            guard isViewLoaded else { return }
+            
             DispatchQueue.main.async {
                 self.tableView.reloadData()
             }
@@ -24,6 +28,10 @@ class VideosViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.backgroundColor = UIColor(hexString: "004D40")
+        tableView.separatorColor = UIColor(hexString: "006654")
+        tableView.separatorInset = .zero
     }
     
     func updateList() {
@@ -31,69 +39,38 @@ class VideosViewController: UITableViewController {
             .filter({ $0.talk != nil })
             .sorted(by: { $0.talk!.id < $1.talk!.id })
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let selectedIndex = tableView.indexPathForSelectedRow else { return }
+        guard let detailViewController = segue.destination as? TalkDetailViewController else { return }
+        
+        let video = videos[selectedIndex.row]
+        detailViewController.talkToShow = video.talk
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+}
+
+extension VideosViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return videos.count
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: Storyboard.cell, for: indexPath)
-
+        
         let video = videos[indexPath.row]
         cell.textLabel?.text = video.talk!.title
-
+        cell.textLabel?.textColor = UIColor.white
+        cell.backgroundColor = UIColor(hexString: "004D40")
+        
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
