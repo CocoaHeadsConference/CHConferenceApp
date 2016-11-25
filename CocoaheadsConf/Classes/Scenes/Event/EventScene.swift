bea2ee657169ceae08d3b223d68f5cd07d243fa9
Bruno Bilescky <brunogb@gmail.com>
2016-11-25 19:44:05 -0200

    Removing unused scenes and adding Event Scene

---
+++ CocoaheadsConf/Classes/Scenes/Event/EventScene.swift
@@ -0,0 +1,20 @@
+//
+//  EventScene.swift
+//  CocoaheadsConf
+//
+//  Created by Bruno Bilescky on 25/11/16.
+//  Copyright © 2016 Cocoaheads. All rights reserved.
+//
+
+import UIKit
+
+class EventScene: Scene {
+    
+    let cache: Cache
+    let initialViewController = UIViewController()
+    
+    init(cache: Cache) {
+        self.cache = cache
+    }
+
+}
