b821ba07165faf06f44af1305ad7e36aaf4961c2
Douglas Taquary <senhordeengenho@hotmail.com>
2019-09-30 22:50:49 -0300

    Initial Commit

---
+++ NSBrazilConf/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NSBrazilConf
+//
+//  Created by resource on 30/09/19.
+//  Copyright © 2019 Cocoaheadsbr. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
