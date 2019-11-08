59b0960ec415f3728bf28d6c5ac10c63afdc59ea
Igor Ferreira <igorcferreira@hotmail.com>
2019-11-08 18:52:36 -0300

    Prepare cache publisher

--- NSBrazilConf/Storage/NSBrazilStore.swift
+++ NSBrazilConf/Storage/NSBrazilStore.swift
@@ -1,10 +1,10 @@
-
 import SwiftUI
 import Combine
 
 public enum FetchError: Error {
     case parse(String)
     case unknown(Error)
+    case invalidCache(Error)
 
     var localizedDescription: String {
         switch self {
@@ -12,6 +12,8 @@ public enum FetchError: Error {
             return String(format: NSLocalizedString("Unable to parse key %@ from json", comment: "Unable to parse fetch activity error"), key)
         case .unknown(let error):
             return String(format: NSLocalizedString("Unable to find info with identifier %@", comment: "info not found error"), error.localizedDescription)
+        case .invalidCache(let error):
+            return String(format: NSLocalizedString("Unable to load device cache %@", comment: "info not found error"), error.localizedDescription)
         }
     }
 }
@@ -24,6 +26,12 @@ public final class NSBrazilStore: ObservableObject {
     public let session: URLSession = URLSession.shared
     let jsonURL: URL = URL(string: "https://nsbrazil.com/app/2019.json")!
 
+    private lazy var contentDecoder: JSONDecoder = {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        return decoder
+    }()
+    
     public var objectWillChange = ObservableObjectPublisher()
     
     public init() {}
@@ -32,20 +40,31 @@ public final class NSBrazilStore: ObservableObject {
     @Published var isLoading: Bool = true
 
     func decode(_ data: Data) -> AnyPublisher<NSBrazil.HomeFeed, FetchError> {
-      let decoder = JSONDecoder()
-      decoder.dateDecodingStrategy = .iso8601
 
-      return Just(data)
-        .decode(type: HomeFeed.self, decoder: decoder)
-        .mapError { error in
-          switch error {
-              case is DecodingError: return FetchError.parse(error.localizedDescription)
-              default: return FetchError.unknown(error)
-          }
-        }
-        .eraseToAnyPublisher()
+        return Just(data)
+            .decode(type: HomeFeed.self, decoder: self.contentDecoder)
+            .mapError { error in
+                switch error {
+                case is DecodingError: return FetchError.parse(error.localizedDescription)
+                default: return FetchError.unknown(error)
+                }
+            }.map {
+                self.cache.saveCache(data)
+                return $0
+            }
+            .eraseToAnyPublisher()
     }
-
+    
+    public func fetchCache() -> AnyPublisher<HomeFeed, FetchError> {
+        
+        return Just(self.cache.loadCache() ?? Data())
+            .decode(type: HomeFeed.self, decoder: self.contentDecoder)
+            .mapError { error in
+                return FetchError.invalidCache(error)
+            }
+            .eraseToAnyPublisher()
+    }
+    
     public func fetchInfo() -> AnyPublisher<HomeFeed, FetchError> {
         return session.dataTaskPublisher(for: jsonURL)
             .map { $0.data }
@@ -57,5 +76,3 @@ public final class NSBrazilStore: ObservableObject {
             }.eraseToAnyPublisher()
     }
 }
-
-
