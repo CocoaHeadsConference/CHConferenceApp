1dab15f89e429dc42c1c35d3385a4e2384d69d32
Igor Ferreira <igorcferreira@hotmail.com>
2019-11-08 18:53:34 -0300

    Use cache publisher as fallback

--- NSBrazilConf/ViewModels/FeedViewModel.swift
+++ NSBrazilConf/ViewModels/FeedViewModel.swift
@@ -13,8 +13,13 @@ public class FeedViewModel: ObservableObject {
     }
 
     func fetchInfo() {
-        isLoading = true
-        cancellable = store.fetchInfo()
+        self.isLoading = true
+        
+        let cachePublisher = self.store.fetchCache()
+        let networkPublisher = self.store.fetchInfo()
+        
+        self.cancellable?.cancel()
+        self.cancellable = networkPublisher.catch({ _ in cachePublisher})
             .receive(on: DispatchQueue.main)
             .sink(receiveCompletion: { received in
                 switch received {
@@ -22,7 +27,6 @@ public class FeedViewModel: ObservableObject {
                     print("sim")
                 case .failure(_):
                     print("nao")
-                    self.isEmpty = true
                 }
             }, receiveValue: { value in
                 self.isLoading = false
@@ -33,6 +37,5 @@ public class FeedViewModel: ObservableObject {
 
     @Published var homeFeed: [FeedItem] = []
     @Published var scheduleFeed: [FeedItem] = []
-    @Published var isEmpty: Bool = false
     @Published var isLoading: Bool = false
 }
