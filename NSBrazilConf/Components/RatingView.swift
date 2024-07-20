5fe80d3312c693a627ef36996ad114a2b641279b
lfoliveir4 <luis.felipi@hotmail.com>
2024-07-20 16:28:00 -0300

    initial commit new home

---
+++ NSBrazilConf/Components/RatingView.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+
+struct RatingView: View {
+    @Binding var rating: Int
+    
+    var label = ""
+    var maximumRating = 5
+    
+    var offImage: Image?
+    var onImage = Image(systemName: "star.fill")
+    
+    var offColor = Color.gray
+    var onColor = Color.yellow
+    
+    var body: some View {
+        HStack {
+            if label.isEmpty == false {
+                Text(label)
+            }
+            
+            ForEach(1..<maximumRating + 1, id: \.self) { number in
+                Button {
+                    rating = number
+                } label: {
+                    image(for: number)
+                        .foregroundStyle(number > rating ? offColor : onColor)
+                }
+            }
+        }
+        .buttonStyle(.plain)
+    }
+    
+    func image(for number: Int) -> Image? {
+        return if number > rating {
+            offImage ?? onImage
+        } else {
+            onImage
+        }
+    }
+}
+
+#Preview {
+    RatingView(rating: .constant(3))
+}
---
+++ NSBrazilConf/NSBrazilConfApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct NSBrazilConfApp: App {
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            HomeTabBar(model: FeedViewModel())
+        }
+    }
+}
