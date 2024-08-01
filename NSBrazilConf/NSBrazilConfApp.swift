73d613c3186bae7508104603000011e525ff04a4
Mauricio Cardozo <mauricio.cardozo@me.com>
2024-08-01 14:34:49 -0300

    moves legacy app files to NSBrazilLib

--- NSBrazilConf/NSBrazilConfApp.swift
+++ NSBrazilConf/NSBrazilConfApp.swift
@@ -1,3 +1,4 @@
+import NSBrazilLib
 import SwiftUI
 
 @main
---
+++ NSBrazilLib/Sources/NSBrazilLib/Legacy/HomeList.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct HomeList: View {
+  let feedViewModel: FeedViewModel
+
+  @Environment(\.horizontalSizeClass) var sizeClass
+
+  var body: some View {
+    ScrollView {
+      VStack(alignment: .leading ,spacing: 15) {
+        ForEach(feedViewModel.homeFeed) { item in
+          FeedBuilder.view(for: item)
+        }
+      }
+      .padding(.vertical, 30)
+    }
+  }
+}
+
+#Preview {
+  HomeList(feedViewModel: .mock)
+}
