c976bc2bd693c9b5ff23550ea3a8d1abb30dd1e0
Mauricio Cardozo <mauricio.cardozo@me.com>
2025-05-13 11:48:31 -0300

    mauricio/taco-tuesday

---
+++ CocoaHeadsKit/Sources/CocoaHeadsKit/Hidden/ChapterPicker.swift
@@ -0,0 +1,35 @@
+//
+//  ChapterPicker.swift
+//  CocoaHeadsKit
+//
+//  Created by Mauricio on 5/13/25.
+//
+
+
+struct ChapterPicker: View {
+
+  @Binding var chapter: Chapter?
+  @State private var chapterList: [Chapter] = []
+  @State private var isLoading = false
+
+  @Environment(\.cloudKitService) var cloudKit
+  @Environment(\.dismiss) var dismiss
+
+  var body: some View {
+    List(chapterList) { chapter in
+      Button(chapter.title) {
+        self.chapter = chapter
+        dismiss()
+      }
+    }
+    .task {
+      isLoading = true
+      do {
+        chapterList = try await cloudKit.fetchData()
+        isLoading = false
+      } catch {
+        // TODO: ?
+      }
+    }
+  }
+}
---
+++ CocoaHeadsKit/Sources/CocoaHeadsKit/Hidden/PageCreator/ChapterEditor.swift
@@ -0,0 +1,17 @@
+//
+//  ChapterEditor.swift
+//  CocoaHeadsKit
+//
+//  Created by Mauricio on 5/13/25.
+//
+
+import SwiftUI
+
+struct ChapterEditor: View {
+
+  @Environment(\.cloudKitService) var cloudKit
+
+  var body: some View {
+    Text("")
+  }
+}
